ff3aa8545b150f3be18f5d75e75122a94f80891b
Sherry Chen <shchen@ad.unc.edu>
2024-09-29 02:00:33 -0400

    analysis 1 done pt 2

--- cdc240.py
+++ cdc240.py
@@ -2,12 +2,29 @@ import numpy as np
 import pandas as pd
 
 ds = pd.read_csv('data_SS.csv')
-df = pd.DataFrame()
+df = pd.DataFrame(columns = ["mean", "number of reviews", "number of no reviews"])
+i=1
 
-i = 1
-while i < 25: 
-    rslt_ds =ds[ds[f"Category {i}"] > 0]
-    pd.DataFrame("Category{i}", rslt_ds)
-    i += 1 
-  
-print('\nResult dataframe :\n', df) 
+while i < 25:
+    num_of_review = 0
+    num_not_review = 0
+    mean = 0
+
+    num_of_review = ds[ds[f"Category {i}"] > 0].count(axis="index").loc['User']
+    #before .count -> filters out rows with 0
+    #after .count -> counts the number of rows **AS A SERIES**
+    #.loc -> pulls a specific number from the series so fixes a major issue
+    num_not_review = ds[ds[f"Category {i}"] == 0].count(axis="index").loc['User']
+    #before .count -> filters to only get rows with 0
+    #after .count -> counts the number of rows **AS A SERIES**
+    #.loc -> pulls a specific number from the series so fixes a major issue
+    mean = ds[f"Category {i}"].sum() / num_of_review
+    # .sum -> adds up the row (don't have to filter out 0 cause adding 0 doesn't change anything)
+
+    df.loc[len(df)] = {"mean" : mean, "number of reviews" : num_of_review, "number of no reviews" : num_not_review}
+    #add to the new dataset the mean, number of users who review that type of attraction, number of users who don't review that type of attraction
+
+    i +=1
+    
+
+print('\nResult Dataset:\n', df)
