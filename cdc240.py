dbb490b91642092b0576d6789fcbc549848e0a5d
Sherry Chen <shchen@ad.unc.edu>
2024-09-29 01:51:22 -0400

    First Analysis Complete

--- cdc240.py
+++ cdc240.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 
 ds = pd.read_csv('data_SS.csv')
+df = pd.DataFrame()
 
-banana = "banana"
-
+i = 1
+while i < 25: 
+    rslt_ds =ds[ds[f"Category {i}"] > 0]
+    pd.DataFrame("Category{i}", rslt_ds)
+    i += 1 
+  
+print('\nResult dataframe :\n', df) 
